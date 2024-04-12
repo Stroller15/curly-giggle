6684a77f142e7762cf0fa92092ade69f25e7832b
Stroller15 <shubhamverma6351@gmail.com>
2024-04-12 12:45:31 +0530

    organize utils for response, error, wrap async handler

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,17 @@
 import * as dotenv from 'dotenv';
 import connectDB from "./db/index.js";
+import { app } from './app.js';
+
 
 dotenv.config({path: './env'})
 
-
-
-
-connectDB();
+connectDB()
+.then(() => {
+    // console.log(res)
+    app.listen(process.env.PORT || 8000 , () => {
+        console.log(`server running or port: ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log('Mongodb connection failed!!!', err)
+})
---
+++ server/src/utils/asyncHandler.js
@@ -0,0 +1,8 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler).catch((err) => next(err))
+    }
+}
+
+
+export {asyncHandler}
